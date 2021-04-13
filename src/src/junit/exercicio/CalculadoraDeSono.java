85d100660a0d09da4819c599a86051a76589b100
luiz fernando <luiz123jfmg@gmail.com>
2021-04-12 22:54:50 -0300

    Criando classe exercicio - Calcular horario de sono

---
+++ src/src/junit/exercicio/CalculadoraDeSono.java
@@ -0,0 +1,53 @@
+package src.junit.exercicio;
+
+public class CalculadoraDeSono {
+
+	private Integer idade;
+	private Integer horasDormidas;
+	
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public Integer getHorasDormidas() {
+		return horasDormidas;
+	}
+	public void setHorasDormidas(Integer horasDormidas) {
+		this.horasDormidas = horasDormidas;
+	}
+	
+	public String calcularSono() {
+		
+		if(idade > 0 && idade < 5) {
+			if(horasDormidas >= 10 && horasDormidas <= 14) {
+				return "Sono ideal para crianças de 1 a 5 anos";
+			}else {
+				return "Sono desregulado para crianças de 1 a 5 anos";
+			}
+		}else if(idade < 13) {
+			if(horasDormidas >= 9 && horasDormidas <= 11) {
+				return "Sono ideal para crianças de 6 aos 13 anos";
+			}else {
+				return "Sono desregulado de 6 aos 13 anos";
+			}
+		}else if(idade < 17) {
+			if(horasDormidas >= 8 && horasDormidas <= 9) {
+				return "Sono ideal para adolescentes de 14 a 17 anos";
+			}else {
+				return "Sono desregulado adolescentes de 14 a 17 anos";
+			}
+		}else if(idade < 64) {
+			if(horasDormidas >= 7 && horasDormidas <= 9) {
+				return "Sono ideal para adultos de 18 a 64 anos";
+			}else {
+				return "Sono desregulado para adultos de 18 a 64 anos";
+			}
+		}else {
+			return "idade inválida";
+		}
+		
+	}
+	
+}
--- src/src/junit/exercicio/JogoBicho.java
+++
@@ -1,5 +0,0 @@
-package src.junit.exercicio;
-
-public class JogoBicho {
-
-}
